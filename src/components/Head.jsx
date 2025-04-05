1ee8e347864482bc8f0667ad86290a1bb13ad5a3
Priyanshu Singh <singhpriyanshu086@gmail.com>
2025-04-05 10:36:17 +0530

    ``feat:`` `Header`

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,13 +1,23 @@
 import React from "react";
-import { HamburgerMenu } from "../constants/constant";
-
+import { HamburgerMenu, UserIcon } from "../constants/constant";
+import Logo from "../assets/Logo.webp";
 
 const Head = () => {
-  return <div>
-    <div className="bg-cover">
-      <img src={HamburgerMenu} alt="HamburgerMenu"></img>
+  return (
+    <div>
+      <div className="bg-cover">
+        <img src={HamburgerMenu} alt="HamburgerMenu"></img>
+        <img src={Logo} alt="Logo" />
+      </div>
+      <div>
+        <input type="text" />
+        <button>Search</button>
+      </div>
+      <div>
+        <img src={UserIcon} alt="user" />
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Head;
--- src/constants/constant.jsx
+++ src/constants/constant.jsx
@@ -1,1 +1,5 @@
-export const HamburgerMenu = "https://imgs.search.brave.com/LWovN-U11BgbxwwRhKG_OBmWG1bCFm24sFshVY83EW0/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzYxLzQ1LzE3/LzM2MF9GXzI2MTQ1/MTc0M19VN1BFaVRr/T3VlRFpuTzVQaXdt/SlMwaERSSzdONkts/US5qcGc"
+export const HamburgerMenu =
+  "https://imgs.search.brave.com/LWovN-U11BgbxwwRhKG_OBmWG1bCFm24sFshVY83EW0/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzYxLzQ1LzE3/LzM2MF9GXzI2MTQ1/MTc0M19VN1BFaVRr/T3VlRFpuTzVQaXdt/SlMwaERSSzdONkts/US5qcGc";
+
+export const UserIcon =
+  "https://imgs.search.brave.com/XFTZ6OPY1Wvi0irX0oGscPsWsXHusUhwA0BiiyXZvPw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzEwLzc1LzUwLzMx/LzM2MF9GXzEwNzU1/MDMxNTZfZFU1WG1q/aHQ5TWk0aXlxemhs/TG1LVlJoNVp2aG9y/UUkuanBn";
